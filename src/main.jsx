a95a167d29ca38aeddee9860af53bfbace09f2f5
Abhijeetshavant <abhijeetshavant72@gmail.com>
2026-08-24 19:46:55 +0530

    complete2

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { QuoteCartProvider } from "./hooks/useQuoteCart"; // Import the provider
-import "./index.css"; // ✅
+import { QuoteCartProvider } from "./hooks/useQuoteCart";
+import "./index.css"; // Make sure this is the correct CSS file
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <QuoteCartProvider>
-        {" "}
-        {/* Wrap App with the provider */}
         <App />
       </QuoteCartProvider>
     </BrowserRouter>
